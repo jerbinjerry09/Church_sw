4bc1fe046c8b7a5d7f2d0ffbb52c1a32a846ca15
jerbinAvigo <32266198+jerbinAvigo@users.noreply.github.com>
2018-10-21 20:54:47 +0530

    CSS with framework

--- www/index.php
+++ www/index.php
@@ -7,28 +7,59 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Church</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/spectre.css" charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/spectre-exp.css" charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/spectre-icons.css" charset="utf-8"/>
       <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
       <script language="JavaScript" src="js/tamil.js" type="text/JavaScript"></script>
       <script type="text/javascript">
        
     </script>
-    <form id="login-form" action="login_check.php" name="findkural" method="post">
+    <!-- <form id="login-form" action="login_check.php" name="findkural" method="post">
         <label><?php if( isset($_SESSION['login_error'])){
             echo  $_SESSION['login_error'];
         }
         ?></label>
     <label>Username </label>
-    <input type="test" name="username" id="username" />
+    <input type="text" name="username" id="username" />
     <br>
     <label>Password: </label>
     <input type="password" name="pwd" id="pwd" />
   
     <br>
     <input type="submit" name="login" id="login" value="Login" />
-    </form>
+    </form> -->
+    <!-- grid offset example -->
+    <body>
+<div class="container">
+  <div class="columns">
+    <div class="column col-3 col-mx-auto col-xs-12 col-md-6 col-sm-6">
+    <div class="centered text-center"><img width="100px" src="img/logo.jpg"/></div>
+    <div class="centered text-center"><h4>CSI District Church <br/>Muzhucode</h4></div>
+    <form id="login-form" action="login_check.php" name="findkural" method="post">
+                  <div class="form-group ">
+                    <label class="form-label" for="username">Email</label>
+                    <input class="form-input" name="username" id="username" type="text" placeholder="Email">
+                    <?php if( isset($_SESSION['login_error'])){?>
+            <p class="form-input-hint"><?php echo  $_SESSION['login_error'];?> </p>
+            <?php  } ?>
+                  </div>
+                  <div class="form-group">
+                    <label class="form-label" for="input-example-2pwd">Password</label>
+                    <input class="form-input" name="pwd" id="pwd"  type="password" placeholder="Password">
+                  </div>
+                  <div class="form-group ">
+                  
+                    <input class="btn btn-primary centered btn-block" type="submit" name="login" id="login" value="Login" />
+                   
+                  </div>
+                </form>
+    </div>
+  </div>
+</div>
     
 </body>
 </html>
