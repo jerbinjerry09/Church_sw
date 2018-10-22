af25a8b63a6606139c2681d80ff1b31109ccc6ba
jerbinAvigo <32266198+jerbinAvigo@users.noreply.github.com>
2018-10-22 21:07:01 +0530

    ui change in family add

--- www/add_family.php
+++ www/add_family.php
@@ -47,11 +47,11 @@
         });
 
 </script>
-<button id="btAdd">Add</button>
+
 
     <form action="controls/add_family_check.php" name="family_form" method="POST">
 
-        <table id="ttable">
+        <table id="ttable" class="table table-striped table-hover">
             <thead>
                 <th></th>
                 <th>Member Name</th>
@@ -67,44 +67,99 @@
             <tbody id="tbody">
             <tr>
                 <td>1</td>
-                <td><input type="text" name="e_name[]"/></td>
-                <td><input type="text" id="t_name1" name="t_name[]"/></td>
-                <td><input type="text" name="dob[]"/></td>
-                <td><input type="text" name="dobp[]"/></td>
-                <td><select name="relation[]">
-                        <option value="W/o">W/o</option>
+                <td>
+                <div class="form-group">
+  <input class="form-input" type="text" name="e_name[]" id="input-example-1" placeholder="Name">
+</div>
+             </td>
+                <td>
+                <div class="form-group">
+  <input class="form-input" type="text" id="t_name1" name="t_name[]" placeholder="Tamil Name">
+</div>
+                </td>
+                <td>
+                <div class="form-group">
+  <input class="form-input"  type="date" name="dob[]" placeholder="Date of birth">
+</div>
+                <td>
+                <div class="form-group">
+  <input class="form-input" type="date" name="dobp[]" placeholder="Date of baptism">
+</div>
+                </td>
+                <td>
+                <div class="form-group">
+      <select class="form-select"  name="relation[]">
+      <option value="W/o">W/o</option>
                         <option value="S/o">S/o</option>
                         <option value="D/o">D/o</option>
                         <option value="Family Head">Family Head</option>
-                    </select>
+      </select>
+    </div>
                 </td>
-                <td><input type="radio" name="sex[0]" value="Male" />Male <input type="radio" name="sex[0]" value="Female">Female</td>
-                <td><input type="text" name="occupation[]"></td>
-                <td><input type="radio" name="sts[0]" value="late" />Late <input type="radio" name="sts[0]" value="1">Live</td>
+                <td>
+                <div class="form-group">
+  <label class="form-radio form-inline">
+    <input type="radio" name="sex[0]" value="Male" checked=""><i class="form-icon"></i> Male
+  </label>
+  <label class="form-radio form-inline">
+    <input type="radio" name="sex[0]" value="Female"><i class="form-icon"></i> Female
+  </label>
+</div>
+             
+                <td>
+                <div class="form-group">
+  <input class="form-input" type="text" name="occupation[]" placeholder="Occupation">
+</div>
+                </td>
+                <td>
+                <div class="form-group">
+  <label class="form-radio form-inline">
+    <input type="radio" name="sts[0]" value="late" checked=""><i class="form-icon"></i> Late
+  </label>
+  <label class="form-radio form-inline">
+    <input type="radio" name="sts[0]" value="1"><i class="form-icon"></i> Live
+  </label>
+</div>
+                
+                
+                </td>
             </tr>
             
             </tbody>
         </table>
-        <table>
+        
+        <button type="button" id="btAdd" class="btn btn-primary btn-lg mt-2 centered"><i class="icon icon-plus"></i> Add</button>
+        <table class="table table-striped mt-2">
             <tr>
                 <th>Area</th>
                 <th>Phone Number</th>
                 <th>Address</th>
             </tr>
             <tr>
-                <td><select name="place">
-                        <option value="-">Select an Area</option>
+                <td>
+                <div class="form-group">
+      <select class="form-select"  name="place">
+      <option value="-">Select an Area</option>
                         <option value="2-place1">place1</option>
                         <option value="3-place2">place2</option>
                         <option value="4-place3">place3</option>
                         <option value="5-place4">place4</option>
                         <option value="6-place5">place5</option>
-                    </select>
+      </select>
+    </div>
+                
+               
                 </td>
-                <td><input type="text" name="phone"></td>
-                <td><textarea name="address"></textarea></td>
+                <td><div class="form-group">
+  <input class="form-input" type="text" name="phone" placeholder="Phone number">
+</div></td>
+                <td>
+                <div class="form-group">
+  <textarea class="form-input" id="input-example-3" placeholder="Textarea" rows="3" name="address"></textarea>
+</div></td>
             </tr>
         </table>
-        <input type="submit" name="" value="Submit">
+        <input type="submit" class="btn btn-primary btn-lg mt-2 centered" name="" value="Submit">
     </form>
+
     <?php include('footer.php'); ?>
